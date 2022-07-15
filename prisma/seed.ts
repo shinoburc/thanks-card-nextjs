f9d4f713e1502baee7db37a3ffba3371e82f1a56
shinoburc <dandydot@gmail.com>
2022-07-15 17:36:47 +0900

    add ThanksCard model.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -22,6 +22,28 @@ async function main() {
     },
   });
   console.log({ admin, user });
+
+  const tc1 = await prisma.thanksCard.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      title: "thankscard1 title",
+      body: "thankscard1 body",
+      fromId: admin.id,
+      toId: user.id,
+    },
+  });
+  const tc2 = await prisma.thanksCard.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      title: "thankscard2 title",
+      body: "thankscard2 body",
+      fromId: user.id,
+      toId: admin.id,
+    },
+  });
+  console.log({ tc1, tc2 });
 }
 
 main()
