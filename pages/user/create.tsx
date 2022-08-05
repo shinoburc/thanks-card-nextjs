2d91245d2737a23832195f5b1a428cd5061042fa
shinoburc <dandydot@gmail.com>
2022-08-05 11:03:17 +0900

    add user create page.

---
+++ pages/user/create.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import React from "react";
+
+const UserCreate: NextPage = () => {
+  return (
+    <>
+      <p>User Create</p>
+    </>
+  );
+};
+
+export default UserCreate;
