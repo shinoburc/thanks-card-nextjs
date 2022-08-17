bf56a7ac494a4122f91b9031df53723abf1e4026
shinoburc <dandydot@gmail.com>
2022-08-17 13:43:54 +0900

    fix

--- components/thanks_card/list.tsx
+++ components/thanks_card/list.tsx
@@ -3,11 +3,13 @@ import useSWR from "swr";
 import { Prisma } from "@prisma/client";
 
 /* ライブラリ Material-UI が提供するコンポーネントの import */
+import TableContainer from "@mui/material/TableContainer";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 import { fetcher } from "@/utils/fetcher";
 
@@ -33,34 +35,36 @@ function ThanksCardList(props: Props) {
   if (!thanks_cards) return <div>Loading...</div>;
 
   return (
-    <Table size="small" variant="contained">
-      <TableHead>
-        <TableRow>
-          <TableCell>id</TableCell>
-          <TableCell>title</TableCell>
-          <TableCell>body</TableCell>
-          <TableCell>from</TableCell>
-          <TableCell>to</TableCell>
-          <TableCell>createdAt</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {/* thanks_cards 全件をテーブル出力する */}
-        {thanks_cards?.map((thanks_card: ThanksCardPayload) => {
-          return (
-            /* 一覧系の更新箇所を特定するために一意となる key を設定する必要がある */
-            <TableRow key={thanks_card.id}>
-              <TableCell>{thanks_card.id}</TableCell>
-              <TableCell>{thanks_card.title}</TableCell>
-              <TableCell>{thanks_card.body}</TableCell>
-              <TableCell>{thanks_card.from.name}</TableCell>
-              <TableCell>{thanks_card.to.name}</TableCell>
-              <TableCell>{thanks_card.createdAt?.toString()}</TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>id</TableCell>
+            <TableCell>title</TableCell>
+            <TableCell>body</TableCell>
+            <TableCell>from</TableCell>
+            <TableCell>to</TableCell>
+            <TableCell>createdAt</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/* thanks_cards 全件をテーブル出力する */}
+          {thanks_cards?.map((thanks_card: ThanksCardPayload) => {
+            return (
+              /* 一覧系の更新箇所を特定するために一意となる key を設定する必要がある */
+              <TableRow key={thanks_card.id}>
+                <TableCell>{thanks_card.id}</TableCell>
+                <TableCell>{thanks_card.title}</TableCell>
+                <TableCell>{thanks_card.body}</TableCell>
+                <TableCell>{thanks_card.from.name}</TableCell>
+                <TableCell>{thanks_card.to.name}</TableCell>
+                <TableCell>{thanks_card.createdAt?.toString()}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
