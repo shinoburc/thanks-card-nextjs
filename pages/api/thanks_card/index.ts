c7f950fb2b4308a25d09e801368f897616edb55c
shinoburc <dandydot@gmail.com>
2022-09-02 17:40:23 +0900

    add protect server side API routes with NextAuth.js.

--- pages/api/thanks_card/index.ts
+++ pages/api/thanks_card/index.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
 
 //import { PrismaClient, ThanksCard } from "@prisma/client";
 //const prisma = new PrismaClient();
@@ -10,6 +11,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ThanksCard[]>
 ) {
+
+  // reference: https://next-auth.js.org/tutorials/securing-pages-and-api-routes
+  const token = await getToken({ req });
+  console.log("token:", token);
+  // Unauthorized
+  if (!token) {
+    res.status(401).end();
+  }
+
   const thanks_cards = await prisma.thanksCard.findMany({
     include: {
       from: true,
