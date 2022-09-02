2737f87a694d00e2c28d8c588d061b2dfade7f89
shinoburc <dandydot@gmail.com>
2022-09-02 17:38:11 +0900

    add RedirectLogin if unauthenticated.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,29 @@
 import "../styles/globals.css";
+import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import React from "react";
+import React, { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Layout from "../layout/Layout";
 
 //function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <RedirectLogin />
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </SessionProvider>
   );
 }
+
+const RedirectLogin: NextPage = () => {
+  const { status } = useSession();
+  if (status === "unauthenticated") {
+    signIn();
+  }
+  return <></>;
+};
 
 export default MyApp;
